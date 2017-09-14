1b963e6c2ed9a89d89807b2c2a84defb607a21f5
Arpitbhavsar <barpit111@gmail.com>
2017-09-14 21:25:59 +0530

    added heade and displayed profile info

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'home#index'
-  
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
